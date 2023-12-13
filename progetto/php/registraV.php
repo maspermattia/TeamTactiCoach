d6a761f0d8db4f06405ef6152bb1f55f27b21a99
maspermattia <101709283+maspermattia@users.noreply.github.com>
2023-12-13 15:13:21 +0000

    fff

--- progetto/php/registraV.php
+++ progetto/php/registraV.php
@@ -1,21 +1,57 @@
 <?php
-$config = [
-    'db_engine' => 'mysql',
-    'db_host' => '127.0.0.1',
-    'db_name' => 'test',
-    'db_user' => 'root',
-    'db_password' => '',
-];
+// Connessione al database 
+$servename = "localhost";
+$username = "root";
+$password = "";
+$dbname = "TeamTactiCoach";
 
-$db_config = $config['db_engine'] . ":host=".$config['db_host'] . ";dbname=" . $config['db_name'];
+$conn = new mysqli($servename, $username, $password, $dbname);
 
-try {
-    $pdo = new PDO($db_config, $config['db_user'], $config['db_password'], [
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-    ]);
-        
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-} catch (PDOException $e) {
-    exit("Impossibile connettersi al database: " . $e->getMessage());
+// Controllo della connessione
+if ($conn->connect_error) {
+    die("Connessione fallita: " . $conn->connect_error);
 }
+
+// Registrazione utente
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $nome = $_POST["nome"];
+    $cognome = $_POST["cognome"];
+    $password = $_POST["password"];
+    $dataDiNascita = $_POST["dataDiNascita"];
+    $mail = $_POST["mail"];
+   
+
+    // Verifica se il nome,email esistono già nel database
+    $verifica = "SELECT * FROM Utente WHERE Username='$username' AND Mail='$mail'";
+    $result = $conn->query($verifica);
+
+    if ($result->num_rows > 0) {
+        echo "Errore: Il nome utente '$username' o l'indirizzo email '$mail' sono già in uso ";
+    } else {
+        
+                }
+            // Hash della password con md5
+                $hashPassword = md5($password);
+
+                // Inserimento dati nel database
+                $sql = "INSERT INTO Utente (Username, Nome, Cognome, Password, DataDiNascita,Mail) 
+                    VALUES ('$username', '$nome', '$cognome', '$hashPassword', '$dataDiNascita', '$mail')";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "Registrazione avvenuta con successo";
+                    header("Location: Login.php");
+                    exit();
+                } else {
+                    echo "Errore: " . $sql . "<br>" . $conn->error;
+                }
+     }  
+      
+
+
+// Chiudi connessione database
+$conn->close();
+
+// Esci dallo script
+exit();
+?>
